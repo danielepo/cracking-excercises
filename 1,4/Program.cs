f1934bb07a69319a27580601750013932bde84c1
danielepo <dnl.pozzobon@gmail.con>
2016-10-12 15:58:08 +0200

    first commit

---
+++ 1,4/Program.cs
@@ -0,0 +1,116 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ex15 = new Ex1_5();
+			Console.WriteLine(ex15.OneAway("hello world", "hello world!"));
+			Console.WriteLine(ex15.OneAway("hello world", "helloworld"));
+			Console.WriteLine(ex15.OneAway("hello world", "hallo world"));
+			Console.WriteLine(ex15.OneAway("hello world", "hallo world!"));
+			Console.WriteLine(ex15.OneAway("hello world", "helloorld!"));
+			Console.WriteLine(ex15.OneAway("", " "));
+			Console.WriteLine(ex15.OneAway(" ", "  "));
+			Console.WriteLine(ex15.OneAway(" ", " "));
+		}
+	}
+	/*
+One Away
+ add one, remove one, change one
+ 
+two strings have to have all equals characters except for one:
+ - add one -> second string has to have one character more
+	"hello word" -> "hello word!"
+ - removce one -> second string hat to have one character missing
+	"hello word" -> "helloword"
+ - change one -> same number of characters but one differnet
+	"hello word" -> "hallo word"
+	
+two different solutions:
+ - count the number of each character and they have to differ only by one in on spot
+	runs one time for each string and then another on the count
+ - swipe the strings with two differnt indexes. 
+	- count the differences found, if more that 1 return false if none return false
+
+	
+bsically we have two pssibilities
+ - the two strings have equal length
+ - the two different lengths 
+ 
+ this would simpify the search algorithm because in the first case i can count the differences and if they differ form 1 returns false
+ in the second case i use the as the index the bigest so I don't have to guard for index out of range
+*/
+
+class Ex1_5
+{
+	public bool OneAway(string text1,string text2)
+	{
+		if(Math.Abs(text1.Length - text2.Length) > 1) 
+			return false;
+			
+		if(text1.Length == text2.Length)
+			return HandleEqualLenght(text1, text2);
+		if(text1.Length > text2.Length)
+			return HandleDifferentLength(text1, text2);
+		else 
+			return HandleDifferentLength(text2, text1);
+	}
+	
+	private bool HandleEqualLenght(string text1, string text2)
+	{
+		var count = 0;
+		for(var i = 0; i < text1.Length; i++)
+		{	
+			if(text1[i] != text2[i])
+				count++;
+		}
+		
+		if(count == 1)
+			return true;
+		return false;
+	}
+	
+	private bool HandleDifferentLength(string long_, string short_)
+	{
+		var count = 0;
+		var j = 0;
+		
+		for(var i = 0; i<long_.Length;i++)//todo invertito l'ordine di lettura degli array
+		{
+			if (j == short_.Length) // todo dimenticato di controllare di non sforare l'array
+				return true;
+			if(long_[i] == short_[j])//todo confronto errato
+			{
+				++j;
+				continue;
+			}
+			++i;
+			if (i == long_.Length) // forgot   
+				return false;
+			if (long_[i] == short_[j])
+			{
+					
+				count++;// todo dimenticato di aggiornare un indice
+			}
+			else
+				return false;
+		
+			++j;
+		}
+		
+		if(count == 1)
+			return true;
+		return false;
+	}
+}
+
+
+}
---
+++ 1.3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3
+{
+    /* p194
+     * la soluzione ideale impiega solo due passaggi (a parte la lungezza che era fornita)
+     * il secondo passaggio di ordinamento viene fatto usando due indici uno per la posizione di lettura ed uno per quella 
+     * di scrittura.
+     * la stringa viene riscritta all'indietro come avevo prevvisto
+     */
+    public class Program
+    {
+        public static void Main()
+        {
+            var s = "Hello World! How are you?          ".ToCharArray();
+
+            Console.WriteLine(Urlify(s));
+        }
+        /*
+    l lughezza array
+    s lungezza stringa
+    */
+        public static char[] Urlify(char[] text) //TODO l'esercizio forniva già la lungezza della stringa, non serviva calcolarla
+        {
+            int strI = FindLastCharacter(text);
+
+            if (strI == -1)
+                return text;
+
+            int spaces = CountSpaces(text, strI);
+
+            SpaceWords(text, spaces, strI);
+            InsertEncoding(text, strI,spaces);
+            return text;
+        }
+
+        // O(l - s)
+        private static int FindLastCharacter(char[] text)
+        {
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                if (text[i] == ' ')
+                    continue;
+                return i;
+            }
+            return -1;
+        }
+
+        // O(s)
+        private static int CountSpaces(char[] text, int strI)
+        {
+            int spaces = 0;
+            for (int i = strI; i >= 0; i--)
+            {
+                if (text[i] == ' ')
+                    spaces++;
+            }
+            return spaces;
+        }
+
+        // O(s)
+        private static void SpaceWords(char[] text, int spaces, int strI)
+        {
+            for (int i = strI; i >= 0; i--)
+            {
+                if (text[i] == ' ')
+                    spaces--;
+                else
+                    MoveCharacter(text, spaces, i);
+            }
+        }
+
+        private static void MoveCharacter(char[] text, int spaces, int index)
+        {
+            var np = index + spaces * 2;
+            text[np] = text[index];
+        }
+
+        private static void InsertEncoding(char[] text, int strI, int spaces)
+        {
+            for (int i = 0; i < strI+spaces*2;i++)//TODO dimenticato una funzionalitá che avevo previsto
+            {
+                if (text[i] == ' ')
+                {
+                    text[i] = '%';
+                    text[++i] = '2';
+                    text[++i] = '0'; // TODO invertito l'operatore di incremento, ++i prima incremeta e poi valuta
+                }
+            }
+        }
+    }
+}
